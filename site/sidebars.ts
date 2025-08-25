cabbd00693b4682743431fc671b02842d3b3933b
Sugu Sougoumarane <ssougou@gmail.com>
2025-08-25 10:14:51 -0700

    Architecture Overview doc (#63)
    * doc: architecture WIP
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>
    
    * doc: arch: fix tense
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>
    
    * doc: address review comments
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>
    
    * doc: add details on upcoming docs
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>
    
    ---------
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- site/sidebars.ts
+++ site/sidebars.ts
@@ -1,7 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 
 const sidebars: SidebarsConfig = {
-  docs: ['index', 'mvp', 'contributing', 'github-workflow'],
+  docs: ['index', 'architecture', 'mvp', 'contributing', 'github-workflow'],
 }
 
 export default sidebars
