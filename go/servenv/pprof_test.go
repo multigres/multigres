b075ef885abacf0418468e49b41f3b59a4e7793a
Rafael Chacon <24160+rafael@users.noreply.github.com>
2025-11-06 13:44:31 -0500

    refactor(servenv): fixes for faster tests
    Signed-off-by: Rafael Chacon <24160+rafael@users.noreply.github.com>

--- go/servenv/pprof_test.go
+++ go/servenv/pprof_test.go
@@ -1,25 +1,18 @@
-//go:build !race
-
-/*
-Copyright 2019 The Vitess Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Modifications Copyright 2025 Supabase, Inc.
-*/
-
-// Disabling race detector because it doesn't like TestPProfInitWithWaitSig and TestPProfInitWithoutWaitSig,
-// but the profileStarted variable is updated in response to signals invoked in the tests and works as intended.
+// Copyright 2019 The Vitess Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Modifications Copyright 2025 Supabase, Inc.
 
 package servenv
 
@@ -31,7 +24,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -91,28 +83,33 @@ func TestPProfInitWithWaitSig(t *testing.T) {
 	sv.pprofFlag.Set(strings.Split("cpu,waitSig", ","))
 
 	sv.pprofInit()
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(0), profileStarted)
+	require.Eventually(t, func() bool {
+		return !isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err := syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(1), profileStarted)
+	require.Eventually(t, func() bool {
+		return isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err = syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(0), profileStarted)
+	require.Eventually(t, func() bool {
+		return !isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err = syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(1), profileStarted)
+	require.Eventually(t, func() bool {
+		return isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err = syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(0), profileStarted)
+	require.Eventually(t, func() bool {
+		return !isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 }
 
 // without waitSig, we should start with profiling on and toggle off-on-off
@@ -124,21 +121,25 @@ func TestPProfInitWithoutWaitSig(t *testing.T) {
 	sv.pprofFlag.Set(strings.Split("cpu", ","))
 
 	sv.pprofInit()
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(1), profileStarted)
+	require.Eventually(t, func() bool {
+		return isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err := syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(0), profileStarted)
+	require.Eventually(t, func() bool {
+		return !isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err = syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(1), profileStarted)
+	require.Eventually(t, func() bool {
+		return isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 
 	err = syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	require.NoError(t, err)
-	time.Sleep(1 * time.Second)
-	assert.Equal(t, uint32(0), profileStarted)
+	require.Eventually(t, func() bool {
+		return !isProfileStarted()
+	}, 2*time.Second, 50*time.Millisecond)
 }
