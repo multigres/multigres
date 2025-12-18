589591b075956834bd1fdccf9642347aa5c89f21
Rafael Chacon <24160+rafael@users.noreply.github.com>
2025-12-18 16:30:27 -0500

    fix for tests
    Signed-off-by: Rafael Chacon <24160+rafael@users.noreply.github.com>

--- go/test/endtoend/multipooler/rpc_manager_replication_api_test.go
+++ go/test/endtoend/multipooler/rpc_manager_replication_api_test.go
@@ -1673,8 +1673,8 @@ func TestConfigureSynchronousReplication(t *testing.T) {
 
 		// The standby's application_name is constructed as: {cell}_{name}
 		// Use the ServiceID from the setup which is the multipooler name
-		standbyID := makeMultipoolerID("test-cell", setup.StandbyMultipooler.ServiceID)
-		standbyAppName := fmt.Sprintf("test-cell_%s", setup.StandbyMultipooler.ServiceID)
+		standbyID := makeMultipoolerID("test-cell", setup.StandbyMultipooler.Name)
+		standbyAppName := fmt.Sprintf("test-cell_%s", setup.StandbyMultipooler.Name)
 		t.Logf("Using standby application_name from setup: %s", standbyAppName)
 
 		// Configure synchronous replication on primary with remote_apply and actual standby
