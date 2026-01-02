c52a799f8aa6cb87b8f43f931a53a9e8893c57c1
deepthi <388311+deepthi@users.noreply.github.com>
2026-01-02 15:29:08 -0800

    refactor(types): remove ProblemReplicaTimelineDiverged problem code
    Signed-off-by: deepthi <388311+deepthi@users.noreply.github.com>

--- go/multiorch/recovery/types/types.go
+++ go/multiorch/recovery/types/types.go
@@ -53,7 +53,6 @@ const (
 	ProblemReplicaLagging          ProblemCode = "ReplicaLagging"
 	ProblemReplicaMisconfigured    ProblemCode = "ReplicaMisconfigured"
 	ProblemReplicaIsWritable       ProblemCode = "ReplicaIsWritable"
-	ProblemReplicaTimelineDiverged ProblemCode = "ReplicaTimelineDiverged"
 
 	// Non-actionable: if all hosts are down, there is no way we can failover.
 	ProblemPrimaryAndReplicasDead ProblemCode = "PrimaryAndReplicasDead"
